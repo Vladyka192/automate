ebc6f2f748fb8d61a969a647b8cc3fb070aa3567
Vladyka192 <fedorchenko.2008@mail.ru>
2023-08-09 15:09:24 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+                <div id='name'>
+                    ${leads[i]}
+                </div>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+let text = document.getElementById('name').innerHTML;
+  const copyText = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
